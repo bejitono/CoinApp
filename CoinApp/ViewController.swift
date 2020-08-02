230061c7f8d1d4ee6752e16000ec1cab1d7708f9
bejitono <stdemicheli@protonmail.com>
2020-08-02 07:45:22 +0700

    Initial commit.

---
+++ CoinApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinApp
+//
+//  Created by De MicheliStefano on 02.08.20.
+//  Copyright © 2020 SignalLightning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
